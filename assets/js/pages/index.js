35d0eb294965ce6e5164726df13d45122525e344
a random hacker <contact@mosse-security.com>
2018-01-14 20:00:26 +1100

    humanoids pictures

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,22 +1,28 @@
 $(document).ready(function($) {
     jQuery('#banner-slider').slippry({
-    // general elements & wrapper
-    slippryWrapper: '<div class="sy-box pictures-slider" />',
+        // general elements & wrapper
+        slippryWrapper: '<div class="sy-box pictures-slider" />',
 
-    // options
-    adaptiveHeight: false, // height of the sliders adapts to current slide
-    captions: false, // Position: overlay, below, custom, false
+        // options
+        adaptiveHeight: false, // height of the sliders adapts to current slide
+        captions: false, // Position: overlay, below, custom, false
 
-    // pager
-    pager: false,
-    
-    // controls
-    controls: false,
-    autoHover: false,
+        // pager
+        pager: false,
+        
+        // controls
+        controls: false,
+        autoHover: false,
 
-    // transitions
-    transition: 'kenburns', // fade, horizontal, kenburns, false
-    kenZoom: 500,
-    speed: 5000 // time the transition takes (ms)
+        // transitions
+        transition: 'kenburns', // fade, horizontal, kenburns, false
+        kenZoom: 500,
+        speed: 5000 // time the transition takes (ms)
+    });
+
+    //for each element that is classed as 'pull-down', set its margin-top to the difference between its own height and the height of its parent
+    $('.pull-down').each(function() {
+        var $this = $(this);
+        $this.css('margin-top', $this.parent().height() - $this.height())
     });
 });
