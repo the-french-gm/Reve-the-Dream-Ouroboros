457146cf66dc70943f7b7ad87cc970f95ad0000c
a random hacker <contact@mosse-security.com>
2018-01-16 19:38:34 +1100

    proudarm

---
+++ _includes/rdd-js/templates/classes/proudarm.js
@@ -0,0 +1,46 @@
+RDDJS.templates["proudarm"] = {
+    "settings" : {
+        "high-dreamer" : false,
+        "max-size" : 15,
+        "size" : getRandomInt(13, 15),
+        "2-handed-mace" : +5,
+        "dodging" : +5
+    },
+
+    "characteristics" : [
+        "constitution",
+        "strength",
+        "agility"
+    ],
+
+    "primary-skills" : [
+        "2-handed-mace",
+        "dodging",
+        "jumping",
+        "running",
+        "outdoor-survival"
+    ],
+
+    "exclude" : [
+        "oneiros",
+        "thanatos",
+        "hypnos",
+        "narcos",
+        "acting",
+        "riding",
+        "masonry",
+        "music",
+        "jewelrysmithing",
+        "pickpocket",
+        "alchemy",
+        "astrology",
+        "botany",
+        "legends",
+        "medicine",
+        "writing",
+        "zoology",
+        "locksmithing",
+        "city-survival",
+        "naviguation"
+    ]
+}
