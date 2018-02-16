a6bde28844947677b0d59d06bda7f093b82c7596
Your Name <you@example.com>
2018-02-16 21:14:08 +1100

    fixing bugs and tweaking things

--- assets/js/pages/tools/npc-generator.js
+++ assets/js/pages/tools/npc-generator.js
@@ -25,16 +25,40 @@ function generateHumanoidNPCCard(character, uid, ignore_icons = false) {
     html += '<tr><td>Missile</td><td>:</td><td class="value">'+character['characteristics']['missile']+'</td></tr>'
     html += '<tr><td>Throw</td><td>:</td><td class="value">'+character['characteristics']['throw']+'</td></tr>'
     html += '<tr><td>DM</td><td>:</td><td class="value">'+character['characteristics']['damage-modifier']+'</td></tr>'
+    
+    var keys = Object.keys(character["resolutions"]);
+    keys = RDDJS.utils.shuffleArray(keys);
+    var max = (keys.length >= 4) ? 4 : keys.length;
+    var j = 0;
 
-    $.each(character["resolutions"], function(key, value) {
+    for(var i = 0; i < max; i++) {
+        j++;
+
+        // we add a contingency in case something goes wrong ;)
+        if(j > 10) {
+            break;
+        }
+
+        key = keys[i];
+
+        // just in case!
+        if(!key) {
+            break;
+        }
+
+        value = character["resolutions"][key];
+
         // We don't bother displaying skills that are -X
         if(value.indexOf('--') != -1) {
-            return;
+            if(i > 0) {
+                i--;
+            }
+            continue;
         }
 
         key = key.replace('-', ' ')
         html += '<tr><td class="text-capitalize">'+key+'</td><td>:</td><td class="value">'+value+'</td></tr>'
-    });
+    }
     
     html += '</table>'
     html += '</div>'
